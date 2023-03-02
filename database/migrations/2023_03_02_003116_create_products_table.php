7aa809d7098a1184896ae8f652dc61d5ceafa36a
nicksonzx6r <nicksonzx6r@gmail.com>
2023-03-02 02:41:40 +0200

    in tabelul products am adaugat cimpurile : name, description , price

--- database/migrations/2023_03_02_003116_create_products_table.php
+++ database/migrations/2023_03_02_003116_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('descriptions');
+            $table->decimal('price', 8, 2)->nullable();
             $table->timestamps();
         });
     }
